1b6f990fc790019dfe5e78b45ac99425d2238a3b
Christopher Catton <umcattoc@muskrat.cs.umanitoba.ca>
2014-02-05 15:49:38 -0600

    added iOS project.

---
+++ iOS/NBGI/NBGI/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  NBGI
+//
+//  Created by Christopher Catton on 2014-02-05.
+//  Copyright (c) 2014 Christopher Catton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
