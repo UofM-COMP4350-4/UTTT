e2b2ad78d35b9fa680ac1c8a6191dae7eadc4594
Darthlarrious <laurentgratton@gmail.com>
2014-02-06 23:16:14 -0600

    Create schema.sql
    Added the database schema to github. file created by Cameron

---
+++ server/schema.sql
@@ -0,0 +1,41 @@
+CREATE DATABASE Games_Users;
+
+CREATE TABLE Games (
+	gameID BIGINT NOT NULL AUTO_INCREMENT,
+	gameName VARCHAR(100),
+	PRIMARY KEY(gameID)
+);
+
+CREATE TABLE Users (
+	userID BIGINT NOT NULL AUTO_INCREMENT,
+	userName VARCHAR(100),
+	isOnline BIT,
+	avatarURL VARCHAR(100),
+	PRIMARY KEY(userID)
+);
+
+CREATE TABLE Friends (
+	userID BIGINT NOT NULL,
+	friendID BIGINT NOT NULL,
+	FOREIGN KEY(userID) REFERENCES Users(userID),
+	FOREIGN KEY(friendID) REFERENCES Users(userID),
+	PRIMARY KEY(userID, friendID)
+);
+
+CREATE TABLE Matches (
+	instanceID BIGINT NOT NULL AUTO_INCREMENT,
+	userID BIGINT NOT NULL,
+	gameID BIGINT NOT NULL,
+	FOREIGN KEY(userID) REFERENCES Users(userID),
+	FOREIGN KEY(gameID) REFERENCES Games(gameID),
+	PRIMARY KEY(instanceID)
+);
+
+CREATE INDEX Matches_UserID
+ON Matches (userID);
+
+CREATE INDEX Matches_GameID
+ON Matches (gameID);
+
+CREATE INDEX Users_IsOnline
+ON Users (isOnline);
