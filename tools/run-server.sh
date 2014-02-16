91c89e9fa35e8b115b9b3980d2ef89a29eb42a6d
Jason Robitaille <jason@canuckcoding.ca>
2014-02-16 07:38:09 -0600

    Added scripts to run the server with parameter options to specify port and ip

---
+++ tools/run-server.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# functions
+
+function pause(){
+   read -n 1 -p "Press any key to continue..."
+}
+
+# mainline
+
+# the folder this script is in
+TOOLS="$(cd `dirname "$0"`; pwd)"
+
+# git repository root
+ROOT="`dirname "$TOOLS"`"
+
+cd "$ROOT"
+echo " "
+
+if [ "$1" != "" ] ; then
+	PORT = $1
+fi
+
+if [ "$2" != "" ] ; then
+	IP = $2
+fi
+
+node "$ROOT/server/server.js"
+
+echo " "
+echo "Complete!"
+echo " "
+pause
+echo " "
