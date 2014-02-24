b2a272d0112449ed6d2aedfc65fdb4c3455da98e
Jason Robitaille <robitaille88@gmail.com>
2014-02-24 00:37:46 -0600

    Removed duplicate draggable key

--- client-web/source/views/AppView.js
+++ client-web/source/views/AppView.js
@@ -3,7 +3,7 @@ enyo.kind({
 	kind: "View",
 	controllerKind: "AppController",
 	components:[
-		{name:"upperPanels", kind: "Panels", index:0, arrangerKind: "CollapsingArranger", draggable:true, realtimeFit: true, wrap: false, classes:"panels enyo-fit", draggable:false, onTransitionFinish:"controller.upperTransition", components:[
+		{name:"upperPanels", kind: "Panels", index:0, arrangerKind: "CollapsingArranger", realtimeFit: true, wrap: false, classes:"panels enyo-fit", draggable:false, onTransitionFinish:"controller.upperTransition", components:[
 			{classes:"menu-container"},
 			{name:"lowerPanels", kind:"Panels", index:1, arrangerKind: "CollapsingRightArranger", wrap: false, realtimeFit: true, classes:"main-container enyo-border-box", draggable:false, onTransitionFinish:"controller.lowerTransition", components:[
 				{layoutKind:"FittableRowsLayout", components:[
