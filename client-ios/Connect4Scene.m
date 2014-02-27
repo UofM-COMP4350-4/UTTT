f1f9c82ec8c22dec32e029c1779793d48233aea4
Cameron McKay <cameron0960@gmail.com>
2014-02-26 19:15:21 -0600

    Changed background and text colour.

--- client-ios/Connect4Scene.m
+++ client-ios/Connect4Scene.m
@@ -14,12 +14,13 @@
     if (self = [super initWithSize:size]) {
         /* Setup your scene here */
         
-        self.backgroundColor = [SKColor colorWithRed:0.15 green:0.15 blue:0.3 alpha:1.0];
+        self.backgroundColor = [SKColor colorWithRed:1.0 green:1.0 blue:1.0 alpha:1.0];
         
         SKLabelNode *myLabel = [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
         
         myLabel.text = @"Hello, World!";
         myLabel.fontSize = 30;
+        myLabel.fontColor = [SKColor colorWithRed:0.5 green:0.5 blue:0.5 alpha:1.0];
         myLabel.position = CGPointMake(CGRectGetMidX(self.frame),
                                        CGRectGetMidY(self.frame));
         
@@ -30,7 +31,7 @@
 
 -(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
     /* Called when a touch begins */
-    
+
     for (UITouch *touch in touches) {
         CGPoint location = [touch locationInNode:self];
         
--- client-ios/NBGI/Connect4ViewController.m
+++ client-ios/NBGI/Connect4ViewController.m
@@ -17,8 +17,8 @@
     
     // Configure the view.
     SKView * skView = (SKView *)self.view;
-    skView.showsFPS = YES;
-    skView.showsNodeCount = YES;
+    skView.showsFPS = NO;
+    skView.showsNodeCount = NO;
     
     // Create and configure the scene.
     SKScene * scene = [Connect4Scene sceneWithSize:skView.bounds.size];
