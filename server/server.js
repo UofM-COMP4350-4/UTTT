9496a50241b35b5a40928561934352d92e09666d
unknown <coolchibs@gmail.com>
2014-02-28 01:11:14 -0600

    Fixed Travis errors

--- server/server.js
+++ server/server.js
@@ -31,7 +31,7 @@ server.get("/initialize", function(request, response) {
 	//Setup a Client Id if the id passed was not found in the database
 	//We have to verify that the user id passed is valid (i.e. > 0 or undefined)
 	var id = request.params.userID;
-	if (id > 0 || id == undefined) {
+	if (id > 0 || id === undefined) {
 		//valid values: either the user doesn't have an id or the user does
 		
 		var text = {userID: 1};//send back 1 for every client until we create a new user id for everyone
