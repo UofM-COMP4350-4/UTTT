cdc8296693c5dc9b674dc13f46266df121f890df
root <root@ip-172-31-1-229.us-west-2.compute.internal>
2014-03-01 15:58:41 +0000

    adding base matcmmaker functionality

--- server/models/GameMatchmaker.js
+++ server/models/GameMatchmaker.js
@@ -5,3 +5,31 @@
 // If both users respond to the response within an arbitrary ammount of time, they will be passed to the GameCreator(sp?) opject which will send them the game objects
 
 // the queue runs in an infitite loop and listens on port TBD for the action of the user pressing a play button
+
+var _ = require('underscore');
+var gameQueue = [];
+GameMatchmaker = function() {}; // constructor
+
+GameMatchmaker.joinQueue = function(player,game,callback){
+	newPlayer = {'player':player,'game':game};
+	gameQueue.push(newPlayer);
+	callback(gameQueue);
+};
+
+GameMatchmaker.getGameQueue = function(game,callback){
+	gameQ = _.groupBy(gameQueue,'game')[game];
+	if(gameQ == null){
+		gameQ = [];
+	}
+	callback(gameQ);
+};
+
+GameMatchmaker.totalPlayers = function(callback){
+	callback(gameQueue.length);
+};
+
+GameMatchmaker.queueTotal = function(game,callback){
+	GameMatchmaker.getGameQueue(game, function(q){
+		callback(q.length);	
+	});
+};
