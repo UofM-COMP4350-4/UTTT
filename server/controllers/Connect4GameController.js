f5a6e9ea38c1d18686f26cdf70d599d072020639
Cameron McKay <cameron0960@gmail.com>
2014-03-10 00:00:44 -0500

    Extended EventEmitter and added code to emit events based on the result of a move.

--- server/controllers/Connect4GameController.js
+++ server/controllers/Connect4GameController.js
@@ -1,9 +1,13 @@
 var connect4GameBoard = require("../models/connect4/Connect4GameBoard.js");
 var ValidateObjectController = require("./ValidateObjectController.js");
+var events = require("events");
+
+exports.Connect4GameController.__proto__ = events.EventEmitter.prototype; // inherit from EventEmitter
 
 exports.Connect4GameController = function(gameInfo) {
 	ValidateObjectController.ValidateObject(gameInfo);
 	this.gameBoard = new connect4GameBoard.Connect4GameBoard(gameInfo);
+	events.EventEmitter.call(this);
 };
 
 exports.Connect4GameController.LoadGame = function(game) {
@@ -12,38 +16,32 @@ exports.Connect4GameController.LoadGame = function(game) {
 	return this.gameBoard;
 }
 
-exports.Connect4GameController.prototype.CreateGame = function() {
-	
-};
-
-exports.Connect4GameController.prototype.CompleteGame = function() {
-	
-};
-
 exports.Connect4GameController.prototype.RequestMove = function(move) {	
 	ValidateObjectController.ValidateObject(move);
 	var updatedConnect4Move = this.gameBoard.GetLocationIfDropGamePieceAtCol(move.col);
 	
 	if (this.gameBoard.IsPlayersTurn(move)) {
 		if (updatedConnect4Move != null) {
-			this.gameBoard.PlayMoveOnBoard(move.row, move.col);
+			var error = this.gameBoard.PlayMoveOnBoard(move.row, move.col);
 			
 			if (this.gameBoard.IsWinner()) {
-				// send message to winner and opponent saying the game has been won
+				this.emit('gameWon','winner');
 			}
 			else if (this.gameBoard.IsDraw()) {
-				// send message to both players saying the game is a draw
+				this.emit('gameDraw','draw');
+			}
+			else if (typeof error.err == 'undefined') {
+				this.emit('boardChanged', this.gameBoard.CreateJSONObjectOnGameBoardChanged(undefined));
 			}
 			else {
-				// send message to opponent with updated game board state
+				this.emit('invalidMove', error.err);
 			}
 		}
 		else {
-			// move is invalid because column is already full
-			// send message to client saying that this is an invalid move
+			this.emit('invalidMove','Invalid Move: Column is already full.');
 		}	
 	}
 	else {
-		// move is invalid because it was made out of turn
+		this.emit('invalidMove','Invalide Move: It is not your turn.');
 	}
 };
