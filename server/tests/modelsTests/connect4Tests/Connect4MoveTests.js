1d626ca651745e2c25c389bac45898dc5cd326c9
Cameron McKay <cameron0960@gmail.com>
2014-03-09 22:58:35 -0500

    Fixed errors in Connect4MoveTests.

--- server/tests/modelsTests/connect4Tests/Connect4MoveTests.js
+++ server/tests/modelsTests/connect4Tests/Connect4MoveTests.js
@@ -10,23 +10,22 @@ describe('Connect4 Test Suite', function(){
 	describe('Connect4 Move Test Class', function() {
 		it('Test: Initialize Valid Data', function() {
 			var player1 = new playerJS.Player(1,'Player 1');
-			var connect4Move = new connect4MoveJS.Player(2,3,player1);
+			var connect4Move = new connect4MoveJS.Connect4Move(2,3,player1);
 			assert.equal(connect4Move.x, 2);
 			assert.equal(connect4Move.y, 3);
-			assert.deepEqual(connect4Move.Player, player1);
+			assert.equal(connect4Move.player, player1);
 		});
 		
 		it('Test: Initialize Null/NaN/Undefined Data', function() {
-			assert.throws(function() { new connect4MoveJS.Player(null, null, null) }, Error);
-			assert.throws(function() { new connect4MoveJS.Player(undefined, undefined, undefined) }, Error);
-			assert.throws(function() { new connect4MoveJS.Player(NaN, NaN, NaN) }, Error);
+			assert.throws(function() { new connect4MoveJS.Connect4Move(null, null, null) }, Error);
+			assert.throws(function() { new connect4MoveJS.Connect4Move(undefined, undefined, undefined) }, Error);
+			assert.throws(function() { new connect4MoveJS.Connect4Move(NaN, NaN, NaN) }, Error);
 		});
 		
 		it('Test: Initialize Invalid Data', function() {
-			assert.throws(function() { new connect4MoveJS.Player('not an int','not an int',1) }, Error);
-			assert.throws(function() { new connect4MoveJS.Player(false,false,new playerJS.Player(1,'Player 1')) }, Error);
-			assert.throws(function() { new connect4MoveJS.Player(1,2,false) }, Error);
-			assert.throws(function() { new connect4MoveJS.Player(1,1,new playerJS.Player(1,'Player 1')) }, Error);
+			assert.throws(function() { new connect4MoveJS.Connect4Move('not an int','not an int',1) }, Error);
+			assert.throws(function() { new connect4MoveJS.Connect4Move(false,false,new playerJS.Player(1,'Player 1')) }, Error);
+			assert.throws(function() { new connect4MoveJS.Connect4Move(1,2,false) }, Error);
 		});
 	});
 });
