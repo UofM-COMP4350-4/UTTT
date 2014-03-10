0473f55f5b2dab1680b98ce5a78eb619cea332b6
cam96 <cameron0960@gmail.com>
2014-03-09 22:50:35 -0500

    Added constructor validation to Connect4Move.  Added tests for Connect4Move.

--- server/models/connect4/Connect4Move.js
+++ server/models/connect4/Connect4Move.js
@@ -1,9 +1,15 @@
+var ValidateObjectController = require("../.././controllers/ValidateObjectController.js");
+
 exports.Connect4Move = function(x, y, player)
 {
+	ValidateObjectController.ValidateObject(player);
+	ValidateObjectController.ValidateNumber(x);
+	ValidateObjectController.ValidateNumber(y);
 	this.x = x;
 	this.y = y;
 	this.player = player;
 };
+
 exports.Connect4Move.prototype.GetPlayer = function()
 {
 	return this.player;
