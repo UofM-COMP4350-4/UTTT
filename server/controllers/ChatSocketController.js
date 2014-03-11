0237ed1ac6c05a95058ba94ac88a818661328a60
Cameron McKay <cameron0960@gmail.com>
2014-03-11 12:19:36 -0500

    Created ChatSocketController

--- server/controllers/ChatSocketController.js
+++ server/controllers/ChatSocketController.js
@@ -3,31 +3,32 @@ var events = require("events");
 var util = require("util");
 var ValidateObjectController = require("./ValidateObjectController.js");
 
-util.inherits(exports.ChatSocketController, events.EventEmitter);
-
 exports.ChatSocketController = function(port) {
 	this.socketIO = io.listen(port);
 	this.SetupConnectionEvents();
 	
 	function SetupConnectionEvents() {
 	this.socketIO.sockets.on('connection', function(socket) {
-		this.socketIO.sockets.on('chatCreated', function(user, gameInstanceID) {
+		socket.on('createChatRoom', function(user, gameInstanceID) {
 			socket.join('chat/' + gameInstanceID);
 		});
+		
+		socket.on('receiveMessage', function(message) {
+			this.emit('receiveMessage', message);
+		});
 	});
+	
+	this.SendDataToUser = function(userID, data) {
+		ValidateObjectController.ValidateNumber(userID);
+		ValidateObjectController.ValidateString(clientSocketIDHashTable[userID]);
+		this.socketIO.sockets.socket(clientSocketIDHashTable[userID]).emit('receivePlayResult', data);
+	};
+	
+	this.SendDataToAllUsersInGame = function(gameInstanceID, data) {
+		ValidateObjectController.ValidateNumber(gameInstanceID);
+		ValidateObjectController.ValidateObject(this.socketIO.sockets.manager.rooms[('/chat/' + gameInstanceID)]);
+		this.socketIO.sockets.in('chat/' + gameInstanceID).emit('receivePlayResult', data);
+	};	
+}
 
-	this.socketIO.sockets.on('receiveMessage', function(move) {
-		this.emit('receiveMessage', move);
-	});
-}
-}
-
-exports.ChatSocketController.prototype.SendDataToUser = function(userID, data) {
-	ValidateObjectController.ValidateNumber(userID);
-	this.socketIO.sockets.socket(clientSocketIDHashTable[userID].id).emit('receivePlayResult', data);
-}
-
-exports.ChatSocketController.prototype.SendDataToAllUsersInGame = function(gameInstanceID, data) {
-	ValidateObjectController.ValidateNumber(gameInstanceID);
-	this.io.sockets.in(room).emit('chat/' + gameInstanceID, data);
-}
+util.inherits(exports.ChatSocketController, events.EventEmitter);
