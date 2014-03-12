aa1d1e074070840b48beaa0c565e0887a4048f2f
Cameron McKay <cameron0960@gmail.com>
2014-03-11 23:06:55 -0500

    Added player object.

---
+++ client-ios/NBGI/Player.m
@@ -0,0 +1,13 @@
+//
+//  Player.m
+//  NBGI
+//
+//  Created by Cameron McKay on 3/11/2014.
+//  Copyright (c) 2014 Christopher Catton. All rights reserved.
+//
+
+#import "Player.h"
+
+@implementation Player
+
+@end
