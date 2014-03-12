0dadf7b696718e811a3f6ea64d56006fca6f2e37
Cameron McKay <cameron0960@gmail.com>
2014-03-12 04:52:27 -0500

    Added tests for creating a json string from a move object

--- client-ios/NBGITests/MoveTests.m
+++ client-ios/NBGITests/MoveTests.m
@@ -23,12 +23,28 @@
     [super tearDown];
 }
 
-- (void)testValidData {
+- (void)testValidData { // objective-c doesn't allow passing of invalid data to this method since there are no objects.
     Move *move = [[Move alloc]initWithPositionAndUserID:CGPointMake(1, 3) userID:5];
     
     XCTAssertEqual([move.userID intValue], 5, "UserID should be 5");
     XCTAssertEqual(move.position.x, (float)1, "Position X should be 1.");
     XCTAssertEqual(move.position.y, (float)3, "Position Y should be 3.");
+}
+
+- (void)testCreateJSONString {
+    Move *move = [[Move alloc]initWithPositionAndUserID:CGPointMake(1, 3) userID:5];
+    NSString *jsonString = [move CreateJSONString];
+    NSData *playerJSONData = [jsonString dataUsingEncoding:NSUTF8StringEncoding];
+    NSError *error = NULL;
+    
+    NSDictionary *jsonNSDict = [NSJSONSerialization JSONObjectWithData:playerJSONData options:NSJSONReadingMutableContainers error:&error];
+    int userID = [[jsonNSDict objectForKey:@"userID"] intValue];
+    int x = [[jsonNSDict objectForKey:@"x"] intValue];
+    int y = [[jsonNSDict objectForKey:@"y"] intValue];
+
+    XCTAssertEqual(userID, 5, "UserID should be 5.");
+    XCTAssertEqual(x, 1, "X should be 1.");
+    XCTAssertEqual(y, 3, "Y should be 3.");
 }
 
 @end
