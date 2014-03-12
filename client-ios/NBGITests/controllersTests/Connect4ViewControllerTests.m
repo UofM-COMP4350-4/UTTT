f5f044a6d2ebea741d53796108ba3dabf6e3458d
Cameron McKay <cameron0960@gmail.com>
2014-03-12 06:33:16 -0500

    Added stub test methods.

---
+++ client-ios/NBGITests/controllersTests/Connect4ViewControllerTests.m
@@ -0,0 +1,44 @@
+//
+//  Connect4ViewControllerTests.m
+//  NBGI
+//
+//  Created by Cameron McKay on 3/12/2014.
+//  Copyright (c) 2014 Christopher Catton. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Connect4ViewControllerTests : XCTestCase
+
+@end
+
+@implementation Connect4ViewControllerTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here; it will be run once, before the first test case.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here; it will be run once, after the last test case.
+    [super tearDown];
+}
+
+- (void)testDrawGameBoard
+{
+    
+}
+
+- (void)testDidReceiveEvent
+{
+    
+}
+
+- (void)testInitializeGameBoard
+{
+    
+}
+
+@end
