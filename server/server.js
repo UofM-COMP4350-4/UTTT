985c3d54f6af48fcdfcef19e70d2f61abb151b13
Jason Robitaille <jason.robitaille@lge.com>
2014-03-13 01:49:49 -0500

    Syntax fixes

--- server/server.js
+++ server/server.js
@@ -16,7 +16,6 @@ server.get("/queueForGame", function(request, response, next){
 	//We need to Setup a new game in the database between two players
 	//Create a flat file for the board data
 	//Then send back the board object
-	console.log("Queue request received from Client " + request.params.fun);
 	response.writeHead(200, {"content-type": "application/json"});
 	response.end(JSON.stringify({}));
 	return next();
@@ -41,22 +40,16 @@ server.get("/initialize", function(request, response, next) {
 	console.log("Received initialize request from Client " + request.params.userID); 
 	//Setup a Client Id if the id passed was not found in the database
 	var text = {};
-	console.log('Server gets here 1: ' + id);
 	//call DataStoreController and get a the user's information
 	dbController.getUserInformation(request.params.userid, function(newUserInfo){
 		var userInfo = newUserInfo[0];
-		console.log('Server gets here 2: ' + userInfo.userID);
 		//Get a list of games on the server
 		gameMGMT.availableGames(function(gameList){
 			var games = gameList;
-			console.log("List of games returns " + games);
 			//get a list of active games for the user
 			gameMGMT.findByUser(userInfo.userID, function(activeGames){					
 				//for now, send back the user and game list
-				console.log("Game MGMT returns " + activeGames);
-				debugger
-				text = {'user': userInfo, 'availableGames': games, 'active': activeGames};
-								
+				text = {'user': userInfo, 'availableGames': games, 'active': activeGames};		
 				response.writeHead( 200, {'content-type': 'application/json', 'Access-Control-Allow-Origin' : '*'});
 				response.write(JSON.stringify(text));
 				response.end();
@@ -68,8 +61,6 @@ server.get("/initialize", function(request, response, next) {
 
 server.get("/listOfGames", function(request, response, next)
 {
-	console.log("List of games request received from the Client");
-	
 	//Validate the dbController Object
 	gameMGMT.availableGames(function(games){
 		response.writeHead(200, {"content-type": "application/json", 'Access-Control-Allow-Origin' : '*'});
