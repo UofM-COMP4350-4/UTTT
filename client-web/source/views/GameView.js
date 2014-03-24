ea05bfdaac956f5d40cdfb0a025fd42390b93e92
Jason Robitaille <robitaille88@gmail.com>
2014-03-23 23:18:46 -0500

    Added waiting-for-opponent UI rendering block and connection failure rendering block

--- client-web/source/views/GameView.js
+++ client-web/source/views/GameView.js
@@ -9,5 +9,13 @@ enyo.kind({
 				{name:"title", content:"Game"}
 			]}
 		]}
+	]},
+	waitingForOpponent: {waiting:true, classes:"waiting-splash full", components:[
+		{classes:"waiting-splash-title", content:""},
+		{style:"height:15px;"},
+		{classes:"waiting-splash-spinner"}
+	]},
+	connectionFailure: {classes:"full", components:[
+		{classes:"connection-failure", content:"Connection Failed!"},
 	]}
 });
