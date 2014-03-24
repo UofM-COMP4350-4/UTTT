44083bf76a71486b90a27ab2059cf472d6eac8ab
Jason Robitaille <robitaille88@gmail.com>
2014-03-23 23:19:06 -0500

    Added a scroller, moved stylings to css classes

--- client-web/source/views/MenuView.js
+++ client-web/source/views/MenuView.js
@@ -4,15 +4,15 @@ enyo.kind({
 	controllerKind: "MenuController",
 	layoutKind:"FittableRowsLayout",
 	components:[
-		{fit:true, components: [
-			{style:"font-size: 2em; color:silver; padding: 10px 10px 10px 30px;", content:"Let's Play"},
-			{tag:"hr", style:"margin-bottom:5px;"},
+		{kind:"Scroller", fit:true, components: [
+			{classes:"app-title", content:"Let's Play"},
+			{style:"box-shadow: inset 0px -5px 10px 0px rgba(0, 0, 0, 0.5);height:5px;"},
 			{kind:"Drawer", open:false, components:[
-				{kind: "onyx.Groupbox", components: [
+				{kind: "onyx.Groupbox", style:"padding:5%;", components: [
 					{kind: "onyx.GroupboxHeader", content: "Active Matches"},
-					{kind: "Repeater", count: 0, onSetupItem:"controller.setupActiveGames", components: [
+					{kind: "Repeater", count: 0, classes:"active-container", onSetupItem:"controller.setupActiveGames", components: [
 						{name:"matchLink", kind:"Link", style:"width:90%; margin:5%;", components:[
-							{kind:"onyx.Button", ontap:"openMatch", style:"width:100%;", components:[
+							{kind:"onyx.Button", ontap:"controller.switchToGame", classes:"menu-button", components:[
 								{name:"lbl1", content:"Game", classes:"menubtn-title"},
 								{tag:"br"},
 								{name:"lbl2", content:"vs ????", classes:"menubtn-subtitle"}
@@ -22,9 +22,9 @@ enyo.kind({
 				]}
 			]},
 			{style:"padding:30px;", components:[
-				{kind:"onyx.Button", content:"New Match", ontap:"controller.newMatch", style:"width:100%; background-color:#4c4c4c; color: lightgray;"},
+				{kind:"onyx.Button", content:"New Match", ontap:"controller.newMatch", classes:"menu-button"},
 				{style:"height:15px;"},
-				{kind:"onyx.Button", content:"Invite to Play", ontap:"controller.invite", style:"width:100%; background-color:#4c4c4c; color: lightgray;"}
+				{kind:"onyx.Button", content:"Invite to Play", ontap:"controller.invite", classes:"menu-button"}
 			]}
 		]},
 		{kind:"onyx.Toolbar", style:"padding-top:4px; padding-bottom:4px; height:46px;", allowHtml:true, content:"&nbsp;"}
