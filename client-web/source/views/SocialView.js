3f8b8aeb99d1eb1a67819e3a70c6d31987e16846
Jason Robitaille <robitaille88@gmail.com>
2014-03-23 22:44:46 -0500

    Switched to a more deneric label

--- client-web/source/views/SocialView.js
+++ client-web/source/views/SocialView.js
@@ -5,7 +5,7 @@ enyo.kind({
 	layoutKind:"FittableRowsLayout",
 	components:[
 		{fit:true, components:[
-			{style:"position:relative; top: 50%; margin-top: -0.5em; color:silver; text-align:center;", content:"Login Coming Soon"}
+			{style:"position:relative; top: 50%; margin-top: -0.5em; color:silver; text-align:center;", content:"[Social Integration Panel]"}
 		]},
 		{kind:"onyx.Toolbar", style:"padding-top:4px; padding-bottom:4px; height:46px;", classes:"social-shadow", allowHtml:true, content:"&nbsp;"}
 	]
