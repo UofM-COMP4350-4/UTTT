0146c2a428e97f02f2c65e84b02ed887fc3180a1
Jason Robitaille <robitaille88@gmail.com>
2014-03-24 01:17:57 -0500

    Now renders in proper aspect ratio, scaled as needed, with a footer status message

--- client-web/source/views/Connect4View.js
+++ client-web/source/views/Connect4View.js
@@ -2,9 +2,14 @@ enyo.kind({
 	name: "Connect4View",
 	kind: "View",
 	controllerKind: "Connect4Controller",
-	COL_SIZE: 7,
+	classes: "connect4-wrapper-outer",
 	components:[
-			{name:"c4Grid", style:"width:100%; height:100%;"}
+		{name:"wrapper", classes:"connect4-wrapper-inner", components:[
+			{name:"c4Grid", classes:"full"},
+			{classes:"connect4-status-wrapper", components:[
+				{name:"status", classes:"connect4-status", content:"&nbsp;", allowHtml:true}
+			]}
+		]}
 	],
 	row: {style:"width:100%; height:16.6666666667%;"},
 	cell: {style:"width:14.2857142857%; height:100%;display:inline-block;"},
